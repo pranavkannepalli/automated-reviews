e49690b5ca81b4c123fae50fd6761439fce33c7b
Pranav Kannepalli <pranav.kannepalli@gmail.com>
2026-06-26 01:14:14 -0700

    fix(temporal): set worker namespace

--- packages/temporal/src/worker.ts
+++ packages/temporal/src/worker.ts
@@ -1,13 +1,14 @@
 import { NativeConnection, Worker } from "@temporalio/worker";
 
 import * as activities from "./activities";
-import { getTemporalWorkerConnectionOptions } from "./config";
+import { getTemporalNamespace, getTemporalWorkerConnectionOptions } from "./config";
 import { REVIEWS_TASK_QUEUE } from "./shared";
 
 async function run() {
   const connection = await NativeConnection.connect(getTemporalWorkerConnectionOptions());
   const worker = await Worker.create({
     connection,
+    namespace: getTemporalNamespace(),
     workflowsPath: new URL("./workflows.ts", import.meta.url).pathname,
     activities,
     taskQueue: REVIEWS_TASK_QUEUE,
